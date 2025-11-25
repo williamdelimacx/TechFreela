987a3c8fda0a1f1a102a74ac06535adb37513dca
William de Lima <williamdelimacx@gmail.com>
2025-11-25 17:33:33 -0300

    feat: created projects controller

---
+++ TechFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechFreela.API.Controllers;
+
+[Route("api/projects")]
+public class ProjectsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
